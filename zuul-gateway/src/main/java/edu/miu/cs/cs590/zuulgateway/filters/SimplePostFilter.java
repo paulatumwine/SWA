65b9f4ff577d2d1e52deb24e1167bc3a814354e8
Paul A. Tumwine <paulatumwine@gmail.com>
2020-12-22 22:18:38 -0600

    add pre and post filters to zuul gateway

---
+++ zuul-gateway/src/main/java/edu/miu/cs/cs590/zuulgateway/filters/SimplePostFilter.java
@@ -0,0 +1,36 @@
+package edu.miu.cs.cs590.zuulgateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class SimplePostFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        HttpServletResponse response = ctx.getResponse();
+        System.out.println(response.getStatus() + " response from " + request.getRequestURL().toString());
+        return null;
+    }
+}
---
+++ zuul-gateway/src/main/java/edu/miu/cs/cs590/zuulgateway/filters/SimplePreFilter.java
@@ -0,0 +1,34 @@
+package edu.miu.cs.cs590.zuulgateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class SimplePreFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        System.out.println(request.getMethod() + " request to " + request.getRequestURL().toString());
+        return null;
+    }
+}
